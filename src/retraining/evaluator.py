f051fb1790b6aab3527cb4aad8fedd5d9802fbf7
pranavsrinivasvenkatesh <pranav10srinivas@gmail.com>
2026-04-18 21:27:17 +0530

    Update CI pipeline

--- src/retraining/evaluator.py
+++ src/retraining/evaluator.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Any
 
-import numpy as np
+#import numpy as np
 from sklearn.metrics import (
     accuracy_score,
     f1_score,
--- src/training/train_lr.py
+++ src/training/train_lr.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from pathlib import Path
+#from pathlib import Path
 
 import mlflow
 from sklearn.linear_model import LogisticRegression
